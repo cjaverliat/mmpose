002d5bda673edc18be1cc19a760d6408866d58b1
Charles JAVERLIAT <charles.javerliat@gmail.com>
2025-05-19 17:39:52 +0200

    Replaced timm.models.layers -> timm.layers

--- projects/pose_anything/models/backbones/swin_transformer_moe.py
+++ projects/pose_anything/models/backbones/swin_transformer_moe.py
@@ -11,7 +11,7 @@ import torch.distributed as dist
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.checkpoint as checkpoint
-from timm.models.layers import DropPath, to_2tuple, trunc_normal_
+from timm.layers import DropPath, to_2tuple, trunc_normal_
 
 try:
     from tutel import moe as tutel_moe
